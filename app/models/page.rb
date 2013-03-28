e45412343b553121490e2ccda948b5ab0f741d83
sbackus <Samuel.Backus@gmail.com>
2013-03-28 12:25:51 -0400

    added Scrapbook/Page ralationship to models

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,4 @@
 class Page < ActiveRecord::Base
   attr_accessible :scrapbook_id
+  belongs_to :parent
 end
--- app/models/scrapbook.rb
+++ app/models/scrapbook.rb
@@ -1,3 +1,4 @@
 class Scrapbook < ActiveRecord::Base
   attr_accessible :name
+  has_many :pages
 end
