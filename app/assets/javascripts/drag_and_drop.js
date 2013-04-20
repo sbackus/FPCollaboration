74ef7c7107c9730eed83db6193e288c651adf9cd
Sam Backus <Samuel.Backus@gmail.com>
2013-04-20 18:52:33 -0400

    add background selector, clean up html and css

--- app/assets/javascripts/drag_and_drop.js
+++ app/assets/javascripts/drag_and_drop.js
@@ -1,13 +1,23 @@
     $(function() {
       $( ".scrap-options .scrap" ).draggable({ appendTo: ".page", helper: "clone", revert: "invalid",
         stop: function(event, ui){
-          ui.helper.clone().appendTo(".page");
+          var scrap = ui.helper.clone();
+          // var pagetop = $(".page").position().top;
+          // var pageleft = $(".page").position().left;
+          // var scraptop = $(".ui-draggable-dragging").position().top;
+          // var scrapleft = $(".ui-draggable-dragging").position().left;
+          // alert("pagetop: " + pagetop+ " pageleft: "+pageleft+" scraptop: " + scraptop+ " scrapleft: "+scrapleft);
+          // scrap.css({"position":"absolute", "top":scraptop-pagetop+"px", "left": scrapleft-pageleft+"px"})
+          scrap.appendTo(".page");
           $( ".page .scrap" ).draggable({ containment: "parent" });
         }
        });
       $( ".page .scrap" ).draggable({ containment: "parent" });
       $( ".page" ).droppable();
       $( "#scrapbook-side-bar" ).accordion();
+      $(".background-button").click(function(){
+        $(".page").css('background-image', 'url(' + $(this).find("img").attr("src")+ ')');
+      })
     });
 
 
