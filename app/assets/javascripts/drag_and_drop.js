9051ef8c67bc8594bf294f678cf5eebba825892a
Sam Backus <Samuel.Backus@gmail.com>
2013-04-20 13:00:45 -0400

    basic drag and drop for scrapbook

--- app/assets/javascripts/drag_and_drop.js
+++ app/assets/javascripts/drag_and_drop.js
@@ -1,8 +1,15 @@
-  $(function() {
-    $( ".draggable" ).draggable();
-    $( ".droppable" ).droppable({
-      drop: function( event, ui ) {
-        //# see http://jqueryui.com/droppable/
-      }
+    $(function() {
+      $( ".scrap-options .scrap" ).draggable({ appendTo: ".page", helper: "clone", revert: "invalid",
+        stop: function(event, ui){
+          ui.helper.clone().appendTo(".page");
+          $( ".page .scrap" ).draggable({ containment: "parent" });
+        }
+       });
+      $( ".page .scrap" ).draggable({ containment: "parent" });
+      $( ".page" ).droppable();
+      $( "#scrapbook-side-bar" ).accordion();
     });
-  });
+
+
+
+
